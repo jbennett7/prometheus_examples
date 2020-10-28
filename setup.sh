4dc8a7024e6a23036facd5468d410794fe31a5a9
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2020-10-27 20:25:21 -0400

    functionized setup.sh

--- setup.sh
+++ setup.sh
@@ -4,21 +4,10 @@
 IP_ADDR=$(ip addr show dev enp0s5|
     sed -ne 's/ *inet \(192.168.50.[0-9]*\).*/\1/p')
 
-wget  https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz
-tar xf prometheus-2.22.0.linux-amd64.tar.gz
-
-wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xf node_exporter-1.0.1.linux-amd64.tar.gz
-
-pushd node_exporter-1.0.1.linux-amd64
-  for i in 0 1 2;do
-      ./node_exporter --web.listen-address ${IPADDR}:808${i} &
-      echo $! > ~/.node_exporter_${i}-run
-  done
-popd
-
-pushd prometheus-2.22.0.linux-amd64
-cat <<EOF > prometheus.yml
+setup_prometheus(){
+  wget  https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz
+  tar xf prometheus-2.22.0.linux-amd64.tar.gz
+  cat <<EOF > prometheus-2.22.0.linux-amd64/prometheus.yml
 global:
   scrape_interval: 15s
   external_labels:
@@ -44,10 +33,28 @@ scrape_configs:
     labels:
       group: 'canary'
 EOF
-cat <<EOF > prometheus.rules.yml
+  cat <<EOF > prometheus-2.22.0.linux-amd64/prometheus.rules.yml
 groups:
 - names: cpu-node
   rules:
   - record: job_instance_mode:node_cpu_seconds:avg_rage5m
     expr: avg by (job, instance, mode) (rate(node_cpu_seconds_total[5m]))
 EOF
+}
+
+setup_node_exporter(){
+  wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+  tar xf node_exporter-1.0.1.linux-amd64.tar.gz
+}
+
+start_node_exporter(){
+  for i in 0 1 2;do
+    ./node_exporter --web.listen-address ${IPADDR}:808${i} &
+    echo $! > ~/.node_exporter_${i}-run
+  done
+}
+
+start_prometheus(){
+  ./prometheus --config.file=prometheus.yml
+  echo $! > ~/.prometheus-run
+}
